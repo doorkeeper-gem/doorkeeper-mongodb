90b0e71eedbd5ec6296ecea6367fffd8113c2ce3
jasl <jasl9187@hotmail.com>
2014-09-07 01:55:55 +0800

    hacking to prevent strange crash on Mongoid 2 and 3

--- lib/doorkeeper/orm/mongoid3/concerns/scopes.rb
+++ lib/doorkeeper/orm/mongoid3/concerns/scopes.rb
@@ -5,6 +5,18 @@ module Doorkeeper
         extend ActiveSupport::Concern
 
         included do
+          def scopes
+            OAuth::Scopes.from_string(self[:scopes])
+          end
+
+          def scopes_string
+            self[:scopes]
+          end
+
+          def includes_scope?(*required_scopes)
+            required_scopes.blank? || required_scopes.any? { |s| scopes.exists?(s.to_s) }
+          end
+          
           def scopes=(value)
             write_attribute :scopes, value if value.present?
           end
