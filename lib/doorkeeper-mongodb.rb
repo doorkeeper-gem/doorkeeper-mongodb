114a83b0f7b4d02a116c282311af60b56ae50b73
jasl <jasl9187@hotmail.com>
2015-11-18 02:27:55 +0800

    mongoid5 support

--- lib/doorkeeper-mongodb.rb
+++ lib/doorkeeper-mongodb.rb
@@ -5,6 +5,7 @@ require 'doorkeeper'
 require 'doorkeeper/orm/mongoid2'
 require 'doorkeeper/orm/mongoid3'
 require 'doorkeeper/orm/mongoid4'
+require 'doorkeeper/orm/mongoid5'
 require 'doorkeeper/orm/mongo_mapper'
 
 module DoorkeeperMongodb
