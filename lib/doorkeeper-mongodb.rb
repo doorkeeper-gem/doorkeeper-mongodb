833296b59bde102bde9cfce0e852467a30b27ee3
fabrice DIDIERJEAN <f.didierjean@ideematic.com>
2016-11-04 14:24:07 +0100

    load mongoid6

--- lib/doorkeeper-mongodb.rb
+++ lib/doorkeeper-mongodb.rb
@@ -6,6 +6,7 @@ require 'doorkeeper/orm/mongoid2'
 require 'doorkeeper/orm/mongoid3'
 require 'doorkeeper/orm/mongoid4'
 require 'doorkeeper/orm/mongoid5'
+require 'doorkeeper/orm/mongoid6'
 require 'doorkeeper/orm/mongo_mapper'
 
 module DoorkeeperMongodb
