31815371047d63c0fe511aa4594ecd7a41fc01a3
fabrice DIDIERJEAN <f.didierjean@ideematic.com>
2016-12-01 14:55:10 +0100

    Replice find_by by where to avoid exception

--- lib/doorkeeper/models/access_token_mixin.rb
+++ lib/doorkeeper/models/access_token_mixin.rb
@@ -47,7 +47,7 @@ module Doorkeeper
       #   if there is no record with such token
       #
       def by_token(token)
-        find_by(token: token.to_s)
+        where(token: token.to_s).first
       end
 
       # Returns an instance of the Doorkeeper::AccessToken
@@ -60,7 +60,7 @@ module Doorkeeper
       #   if there is no record with such refresh token
       #
       def by_refresh_token(refresh_token)
-        find_by(refresh_token: refresh_token.to_s)
+        where(refresh_token: refresh_token.to_s).first
       end
 
       # Revokes AccessToken records that have not been revoked and associated
@@ -172,9 +172,9 @@ module Doorkeeper
       #
       def last_authorized_token_for(application_id, resource_owner_id)
         send(order_method, created_at_desc).
-          find_by(application_id: application_id,
+          where(application_id: application_id,
                   resource_owner_id: resource_owner_id,
-                  revoked_at: nil)
+                  revoked_at: nil).first
       end
     end
 
