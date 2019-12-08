b07c694d3140d673215edd4695cc17ec2cc5053f
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2019-12-08 17:37:12 +0100

    Add bvrypt & actionpack

--- gemfiles/Gemfile.mongoid6.rb
+++ gemfiles/Gemfile.mongoid6.rb
@@ -1,5 +1,7 @@
 gemfile = File.expand_path('../Gemfile.common.rb', __FILE__)
 instance_eval IO.read(gemfile), gemfile
 
+gem 'actionpack', '~> 6.0', '>= 6.0.1'
+gem 'bcrypt', '~> 3.1', '>= 3.1.11'
 gem 'activemodel', '~> 5.2', '< 6'
 gem 'mongoid', '~> 6'
--- gemfiles/Gemfile.mongoid7.rb
+++ gemfiles/Gemfile.mongoid7.rb
@@ -1,5 +1,7 @@
 gemfile = File.expand_path('../Gemfile.common.rb', __FILE__)
 instance_eval IO.read(gemfile), gemfile
 
+gem 'actionpack', '~> 6.0', '>= 6.0.1'
+gem 'bcrypt', '~> 3.1', '>= 3.1.11'
 gem 'activemodel', '~> 6.0', '>= 6.0.1'
 gem 'mongoid', '~> 7.0'
