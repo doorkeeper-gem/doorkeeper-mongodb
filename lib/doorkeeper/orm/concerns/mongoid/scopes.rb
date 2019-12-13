7031fa19851a6dff4c9da692d4d3ea7733cf1d09
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2019-12-13 20:43:35 +0100

    Move module method scope in class method

--- lib/doorkeeper/orm/concerns/mongoid/scopes.rb
+++ lib/doorkeeper/orm/concerns/mongoid/scopes.rb
@@ -7,22 +7,25 @@ module Doorkeeper
 
       included do
         field :scopes, type: String
-      end
 
-      def scopes
-        OAuth::Scopes.from_string(scopes_string).to_s
-      end
+        def scopes
+          OAuth::Scopes.from_string(scopes_string).to_s
+        end
 
-      def scopes=(value)
-        write_attribute :scopes, Array(value).join(" ")
-      end
+        def scopes=(value)
+          # write_attribute :scopes, Array(value).join(" ")
+          super Array(value).join(" ")
+        end
 
-      def scopes_string
-        self[:scopes]
-      end
+        def scopes_string
+          self[:scopes]
+        end
 
-      def includes_scope?(*required_scopes)
-        required_scopes.blank? || required_scopes.any? { |scope| scopes.exists?(scope.to_s) }
+        def includes_scope?(*required_scopes)
+          required_scopes.blank? || required_scopes.any? do |scope|
+            scopes.exists?(scope.to_s)
+          end
+        end
       end
     end
   end
