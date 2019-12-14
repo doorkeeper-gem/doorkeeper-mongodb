467009d980fb18d4a6830a625e0b382c1d3db410
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2019-12-14 13:00:15 +0100

    Change value of module scopes

--- lib/doorkeeper/orm/concerns/mongoid/scopes.rb
+++ lib/doorkeeper/orm/concerns/mongoid/scopes.rb
@@ -3,28 +3,21 @@
 module Doorkeeper
   module Models
     module Scopes
-      extend ActiveSupport::Concern
+      def scopes
+        OAuth::Scopes.from_string(scopes_string)
+      end
 
-      included do
-        field :scopes, type: String
+      def scopes=(value)
+        super Array(value).join(" ")
+      end
 
-        def scopes
-          OAuth::Scopes.from_string(scopes_string).to_s
-        end
+      def scopes_string
+        self[:scopes]
+      end
 
-        def scopes=(value)
-          # write_attribute :scopes, Array(value).join(" ")
-          super Array(value).join(" ")
-        end
-
-        def scopes_string
-          self[:scopes]
-        end
-
-        def includes_scope?(*required_scopes)
-          required_scopes.blank? || required_scopes.any? do |scope|
-            scopes.exists?(scope.to_s)
-          end
+      def includes_scope?(*required_scopes)
+        required_scopes.blank? || required_scopes.any? do |scope|
+          scopes.include?(scope.to_s)
         end
       end
     end
