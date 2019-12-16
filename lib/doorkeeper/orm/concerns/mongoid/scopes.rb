2bcea9f8628dafd7345932447f8173cd5ec37697
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2019-12-16 22:29:06 +0100

    Change type scopes field

--- lib/doorkeeper/orm/concerns/mongoid/scopes.rb
+++ lib/doorkeeper/orm/concerns/mongoid/scopes.rb
@@ -3,21 +3,23 @@
 module Doorkeeper
   module Models
     module Scopes
-      def scopes
-        OAuth::Scopes.from_string(scopes_string)
-      end
+      included do
+        field :scopes, type: Doorkeeper::OAuth::Scopes
 
-      def scopes=(value)
-        super Array(value).join(" ")
-      end
+        def scopes
+          OAuth::Scopes.from_string(scopes_string)
+        end
 
-      def scopes_string
-        self[:scopes]
-      end
+        def scopes=(value)
+          super Array(value).join(" ")
+        end
 
-      def includes_scope?(*required_scopes)
-        required_scopes.blank? || required_scopes.any? do |scope|
-          scopes.include?(scope.to_s)
+        def scopes_string
+          self[:scopes]
+        end
+
+        def includes_scope?(*required_scopes)
+          required_scopes.blank? || required_scopes.any? { |scope| scopes.exists?(scope.to_s) }
         end
       end
     end
